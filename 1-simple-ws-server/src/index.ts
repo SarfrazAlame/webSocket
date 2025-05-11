6111da87f7186082af5fcc36fdd209ba6fb6b963
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-05-11 06:30:33 +0530

    websocket

--- 1-simple-ws-server/src/index.ts
+++ 1-simple-ws-server/src/index.ts
@@ -1,25 +1,53 @@
-import express from "express";
-import http from "http";
-import { WebSocketServer } from "ws";
+// import express from "express";
+// import http from "http";
+// import { WebSocketServer } from "ws";
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-const wss = new WebSocketServer({ server });
+// const wss = new WebSocketServer({ server });
+
+// wss.on("connection", async (ws, req) => {
+//     ws.on("message", (message) => {
+//         console.log("received: %s", message);
+//         ws.send(`Hello, you sent -> ${message}`);
+//     });
+// });
+
+// app.get("/health", (req, res) => {
+//     res.json({ msg: "I am healthy" })
+// })
+
+// server.listen(port, () => {
+//     console.log("start listening...")
+// });
+
+// // /////////////////////////////////////////////////////////////////////  practice
+import express from 'express'
+import { WebSocketServer } from 'ws'
+import http from 'http'
+
+const app = express()
+const port = 3000
+
+const server = http.createServer(app)
+
+const wss = new WebSocketServer({ server })
+
 
 wss.on("connection", async (ws, req) => {
     ws.on("message", (message) => {
-        console.log("received: %s", message);
-        ws.send(`Hello, you sent -> ${message}`);
-    });
-});
+        console.log("message from server", message.toString())
+        ws.send(` you sent ${message}`)
+    })
+})
 
-app.get("/health", (req, res) => {
-    res.json({ msg: "I am healthy" })
+app.get('/health', (req,res)=>{
+    res.send("I am healthy")
 })
 
 server.listen(port, () => {
-    console.log("start listening...")
-});
+    console.log(`server is running`)
+})
